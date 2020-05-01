a4e35918f4371549b0ad9155c786eddef2c79f6e
eoin.payne <eoinpayne@gmail.com>
2020-05-01 07:33:31 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,47 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+
+
+
+class Card extends React.Component {
+  render(){
+    return (
+      <div className = "github-profile">
+        <img src="https://placehold.it/75" />
+        <div className = "info">
+          <div className = "name">{this.props.name}</div>
+          <div className = "company">Company Name here..</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+class GitApp extends React.Component {
+  render(){
+    return (
+      <div>
+        <div className = "header">{this.props.title}</div> 
+        <Card name = "eoin"/>
+    </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <GitApp title = "GitHub card App"/>,
   document.getElementById('root')
 );
+
+
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
